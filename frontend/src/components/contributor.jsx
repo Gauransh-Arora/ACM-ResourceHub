136ae636e6527ae9991343bd2bfb8357e98409a4
Vansh Wadhwa <vanshreads@gmail.com>
2025-08-01 17:31:06 +0530

    Corrected the viewport, prevented display of scrollbars

---
+++ frontend/src/components/contributor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contributor = () => {
+  return (
+    <div className="border-2 border-[#15A6DD] flex items-center gap-96">
+
+
+    </div>
+  )
+}
+
+export default contributor;
