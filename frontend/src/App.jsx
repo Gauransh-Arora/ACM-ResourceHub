bfeaacbfc150a1696e56f2981c73f095b238e9e9
Vansh Wadhwa <vanshreads@gmail.com>
2025-08-03 01:32:00 +0530

    Added json file for year 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ResourcePage from "./pages/ResourcePage";
+import RoadmapPage from "./pages/RoadmapPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/resources" element={<ResourcePage/>} />
+        <Route path="/roadmaps" element={<RoadmapPage/>} />
       </Routes>
     </div>
   );
